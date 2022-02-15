c926a9967a6cb2243df94edce356765ce4a0fee8
schm00g <hk8igureu@relay.firefox.com>
2022-02-15 14:01:58 +0000

    query uniswap v3

---
+++ script.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+
+const main = async () => {
+    try {
+        const result = await axios.post(
+            'https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-v3-subgraph',
+            {
+                query: `{
+                    tokens(first: 10, orderBy: volumeUSD, orderDirection: desc) {
+                        name
+                        symbol
+                        txCount
+                        volumeUSD
+                        feesUSD
+                        totalValueLockedUSD
+                    }
+                }`
+            }
+        );
+    
+        console.log(result.data.data.tokens)
+    } catch (err){
+        console.log(err);
+    }
+}
+
+main();
